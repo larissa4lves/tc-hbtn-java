e77cb0e642f6832146f0e24bf27043a9878a2cff
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-01-27 16:37:17 -0300

    task5

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,14 @@
+public class PrintFormatacaoNumeros {
+	
+	public static void main(String[] args) {
+		float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.printf("Valor:" + String.format("R$ %,.2f",valor) + "\nTaxa: %.2f%%", taxa);
+
+        
+        
+        
+	}
+	
+
+}
