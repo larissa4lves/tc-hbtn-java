d6665fbcb559d77fa30eee92c32d7c959776e0e5
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-01-27 16:05:55 -0300

    task4

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,6 @@ public class PrintStringConcat {
 	public static void main(String[] args) {
 		String texto1 = "Holberton";
 		String texto2 = "School";
-		String string = "Bem vindo a Holberton School!";
 		System.out.print("Bem vindo a " + texto1 + "" + texto2 + '\n');
 	}
 }
