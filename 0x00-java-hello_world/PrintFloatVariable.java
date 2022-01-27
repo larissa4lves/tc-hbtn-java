94179376db375cae4af4032a51ccb54281bf1501
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-01-27 14:22:56 -0300

    task2

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
 	public static void main(String[] args) {
 		float  pi = (float) 3.14159f;
-		System.out.printf("valor de pi: " + "%.3f\n", pi );
+		System.out.printf("Valor de pi: " + "%.3f\n", pi );
 	}
 }
