beda16a9ba1a43b9b2e7bb5e940bdde8beb63524
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-01-27 11:05:50 -0300

    task1

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,7 @@
+public class PrintIntegerVariable {
+	public static void main(String[] args) {
+		String avenida = "Avenida Andaló, ";
+		int numero = 311;
+		 System.out.println(avenida + numero);
+	}
+}
