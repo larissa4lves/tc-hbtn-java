bbe55605e08572d4f33603494930a601361b3a8e
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-01-31 11:55:49 -0300

    task0

--- 0x02/Numero.java
+++ 0x02/Numero.java
@@ -1,23 +1,24 @@
 
 public class Numero {
 	
-public static void dividir(int a, int b) { 
-		
-		int resp = 0;
-		
-		 try {
-			 resp = a/b;
-             
-        }
-        catch(ArithmeticException exeption){
-        	resp = 0;
-        	System.out.println("Nao eh possivel dividir por zero");
-        }
-       	
-        finally {
-        	System.out.println(a + " / " + b + " = " + resp);
-        }
-		}
+public static void dividir(int a, int b) {
+		  
+	       int resp = 0;
+	       
+	       try {
+	    	     resp = a/b;
+	    	     
+	       }
+	       catch(ArithmeticException exeption){
+	    	   resp = 0;
+	    	   System.out.println("Nao eh posivel dividir por zero");
+	       }
+	       
+	       finally {
+	    	   	   System.out.println(a + " / " + b + " = " + resp);
+	       }
+	       
+}
 
 
 }
--- 0x02/Program.java
+++ 0x02/Program.java
@@ -1,19 +1,20 @@
 
 public class Program {
 
+    public static void main(String[] args) {
+        int a, b;
 
-	public static void main(String[] args) {
+        a = 4;
+        b = 3;
+        Numero.dividir(a, b);
+        System.out.println();
+
+        a = 9;
+        b = 0;
+        Numero.dividir(a, b);
+   
+   
     	
-    	 int a, b;
-
-         a = 4;
-         b = 3;
-         Numero.dividir(a, b);
-         System.out.println();
-
-         a = 9;
-         b = 0;
-         Numero.dividir(a, b);
-	}
-
-}
+	
+    }
+    }
