3cca4221b021b767cec11ea9013426f9eb02d334
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-02-02 18:08:44 -0300

    task1

---
+++ 0x05/livraria/exceptions/LivroInvalidoException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class LivroInvalidoException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LivroInvalidoException(String message) {
+		super(message);
+		
+	}
+	
+
+}
