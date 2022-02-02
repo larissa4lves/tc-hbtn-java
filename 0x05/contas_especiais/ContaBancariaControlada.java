b91a9e45e3b18092a93a160cdf4b022aac45ddb8
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-02-02 18:25:58 -0300

    task2

---
+++ 0x05/contas_especiais/ContaBancariaControlada.java
@@ -0,0 +1,23 @@
+public class ContaBancariaControlada extends ContaBancariaBasica {
+	
+	private double saldoMinimo = 0;
+	private double valorPenalidade = 5  ;
+
+	public ContaBancariaControlada(String numeracao, double taxaJurosAnual, double saldoMinimo, double valorPenalidade) {
+		super(numeracao, taxaJurosAnual);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getSaldoMinimo() {
+		return saldoMinimo;
+	}
+
+	public double getValorPenalidade() {
+		return valorPenalidade;
+	}
+
+	@Override
+	 void aplicarAtualizacaoMensal() {
+		this.saldo = saldo - calcularTarifaMensal() + calcularJurosMensal() - valorPenalidade   ;
+	}
+}
