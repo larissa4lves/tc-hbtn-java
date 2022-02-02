3912f87fff6c1ca2db15afecc626945188466bd7
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-02-02 18:48:26 -0300

    task3

---
+++ 0x05/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class OperacaoInvalidaException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OperacaoInvalidaException(String message) {
+		super(message);
+		
+	}
+	
+
+}
