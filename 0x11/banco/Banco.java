e31e8aa7fd5f179dcc28521c41c074362450b932
Larissa Silva Alves <p4ndap0wer24@gmail.com>
2022-02-15 15:22:03 -0300

    task1

---
+++ 0x11/banco/Banco.java
@@ -0,0 +1,28 @@
+
+public class Banco {
+
+	public Banco(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void adicionarAgencia(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void adicionarCliente(String string, String string2, double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void adicionarTransacaoCliente(String string, String string2, double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void listarClientes(String string, boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
